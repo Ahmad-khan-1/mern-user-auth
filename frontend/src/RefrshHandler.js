b5b8628b20b6aef3c0bc93b14e52a0c46aef37ab
Ahmad Mujtaba Aman <qurayshi246@gmail.com>
2025-11-11 10:27:08 +0500

    remove unused react

--- frontend/src/RefrshHandler.js
+++ frontend/src/RefrshHandler.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
 function RefrshHandler({ setIsAuthenticated }) {
   const location = useLocation();
   const navigate = useNavigate();
